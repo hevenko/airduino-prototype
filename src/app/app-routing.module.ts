5e01b8a986602594ec9bd0c7109e7d3b4a4b0e72
hevenko <hevenko@gmail.com>
2020-05-20 11:50:14 +0200

    map comp is showing map

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,8 +1,12 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
+import { MapComponent } from './component/map/map.component';
 
 
-const routes: Routes = [];
+const routes: Routes = [
+  {path: '', redirectTo: '/recipes', pathMatch: 'full' },
+  {path: 'map', component: MapComponent}
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
--- src/app/component/map/map.component.ts
+++ src/app/component/map/map.component.ts
@@ -1,4 +1,9 @@
 import { Component, OnInit } from '@angular/core';
+import Map from 'ol/Map';
+import View from 'ol/View';
+import {defaults as defaultInteractions, DragRotateAndZoom} from 'ol/interaction';
+import TileLayer from 'ol/layer/Tile';
+import OSM from 'ol/source/OSM';
 
 @Component({
   selector: 'app-map',
@@ -6,10 +11,28 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./map.component.css']
 })
 export class MapComponent implements OnInit {
-
+  map: Map;
   constructor() { }
 
   ngOnInit(): void {
+    var customCondition = function (mapBrowserEvent) {
+      console.log(mapBrowserEvent);
+      return false; // TODO
+    };
+    this.map = new Map({
+      interactions: defaultInteractions().extend([
+        new DragRotateAndZoom({ condition: customCondition })
+      ]),
+      layers: [
+        new TileLayer({
+          source: new OSM()
+        })
+      ],
+      target: 'map',
+      view: new View({
+        center: [200, 200],
+        zoom: 2
+      })
+    });
   }
-
 }
