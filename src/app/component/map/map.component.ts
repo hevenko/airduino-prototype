fb69842090c49905290f757b109cfc30b98a1295
hevenko <hevenko@gmail.com>
2020-06-03 11:01:38 +0200

    fix: no map, used setTimeout

--- src/app/component/map/map.component.ts
+++ src/app/component/map/map.component.ts
@@ -1,7 +1,7 @@
 import { Component, OnInit } from '@angular/core';
 import Map from 'ol/Map';
 import View from 'ol/View';
-import {defaults as defaultInteractions, DragRotateAndZoom} from 'ol/interaction';
+import { defaults as defaultInteractions, DragRotateAndZoom } from 'ol/interaction';
 import TileLayer from 'ol/layer/Tile';
 import OSM from 'ol/source/OSM';
 
@@ -14,25 +14,38 @@ export class MapComponent implements OnInit {
   map: Map;
   constructor() { }
 
-  ngOnInit(): void {
-    var customCondition = function (mapBrowserEvent) {
-      console.log(mapBrowserEvent);
-      return false; // TODO
-    };
-    this.map = new Map({
-      interactions: defaultInteractions().extend([
-        new DragRotateAndZoom({ condition: customCondition })
-      ]),
-      layers: [
-        new TileLayer({
-          source: new OSM()
-        })
-      ],
-      target: 'map',
-      view: new View({
-        center: [200, 200],
-        zoom: 2
-      })
-    });
+
+  initMap(ctx) {
+    return new Promise(function (resolve) {
+        setTimeout(() => { // without setTimeout map is empty
+          var customCondition = function (mapBrowserEvent) {
+            console.log(mapBrowserEvent);
+            return false; // TODO
+          };
+          ctx.map = new Map({
+            interactions: defaultInteractions().extend([
+              new DragRotateAndZoom({ condition: customCondition })
+            ]),
+            layers: [
+              new TileLayer({
+                source: new OSM()
+              })
+            ],
+            target: 'map',
+            view: new View({
+              center: [200, 200],
+              zoom: 2
+            })
+          });
+          console.log('before')
+
+          resolve();
+        }, 1);
+      });
+  }
+   async ngOnInit() {
+     await this.initMap(this);
+    console.log('after');
+    
   }
 }
