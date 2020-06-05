3966be8fb442f01154e10e143f7c4de5667540cc
hevenko <hevenko@gmail.com>
2020-06-05 12:20:03 +0200

    message constant

--- src/app/shared/constants.ts
+++ src/app/shared/constants.ts
@@ -1,3 +1,5 @@
 export class Constants {
   static STAY_OPEN = 'stay_open'; // the class prevents the directive from closing component upon date select
+  static MSG_ALREADY_LOGGED_IN = 'Redirecting, already logged in';
+  static MSG_LOGIN_TO_ACCESS = 'Redirecting, please log in to access the page';
 }
--- src/auth-guard.service.ts
+++ src/auth-guard.service.ts
@@ -14,11 +14,13 @@ import { Observable } from 'rxjs';
 import { User } from './app/component/auth/user.model';
 import { Injectable } from '@angular/core';
 import { AuthComponent } from './app/component/auth/auth.component';
+import { MessageService } from './app/shared/service/message.service';
+import { Constants } from './app/shared/constants';
 
 @Injectable()
 export class AuthGuardService implements CanActivate, CanActivateChild {
 
-  constructor(private authService: AuthService, private router: Router) { }
+  constructor(private authService: AuthService, private router: Router, private messageService: MessageService) { }
 
   canActivate(route: ActivatedRouteSnapshot, state: RouterStateSnapshot):
     Observable<boolean> | Promise<boolean> | boolean {
@@ -27,12 +29,20 @@ export class AuthGuardService implements CanActivate, CanActivateChild {
     console.log(this.router);
     if ((<any>route.component).name === 'AuthComponent') {
       if (this.authService.user.value) {
+        this.messageService.messageBus.next([Constants.MSG_ALREADY_LOGGED_IN]);
         return this.router.navigate(['map']);
       } else {
         return true;
       }
     } else {
-      return true;
+      if (this.authService.user.value) {        
+        return true;
+      } else {
+        if ((<any>route.component).name === 'UserListComponent') {          
+          this.messageService.messageBus.next([Constants.MSG_LOGIN_TO_ACCESS]);
+          return this.router.navigate(['map']);
+        }
+      }
     }
   }
   canActivateChild(childRoute: ActivatedRouteSnapshot, state: RouterStateSnapshot):
