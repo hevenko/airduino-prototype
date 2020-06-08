ef16963589c3d92246ab9052544be9a100d34501
hevenko <hevenko@gmail.com>
2020-06-09 00:26:48 +0200

    dot at sentence end

--- src/app/shared/constants.ts
+++ src/app/shared/constants.ts
@@ -1,9 +1,9 @@
 export class Constants {
   static STAY_OPEN = 'stay_open'; // the class prevents the directive from closing component upon date select
-  static MSG_ALREADY_LOGGED_IN = 'Redirecting, already logged in';
-  static MSG_LOGIN_TO_ACCESS = 'Redirecting, please log in to access the page';
-  static MSG_LOGGED_OUT = 'You have been logged out';
-  static MSG_DUPLICATE_MAIL = 'This email exists already';
+  static MSG_ALREADY_LOGGED_IN = 'Redirecting, already logged in.';
+  static MSG_LOGIN_TO_ACCESS = 'Redirecting, please log in to access the page.';
+  static MSG_LOGGED_OUT = 'You have been logged out.';
+  static MSG_DUPLICATE_MAIL = 'This email exists already.';
   static MSG_UNKNOWN_MAIL = 'This email does not exist.';
   static MSG_BAD_PASSWORD = 'This password is not correct.';
 }
