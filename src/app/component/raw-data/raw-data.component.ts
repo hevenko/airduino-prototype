f7936bb14a9cb99e4934f83acccab7bb2735ba68
hevenko <hevenko@gmail.com>
2020-06-16 14:49:23 +0200

    long gps coord.

--- src/app/component/raw-data/raw-data.component.ts
+++ src/app/component/raw-data/raw-data.component.ts
@@ -8,7 +8,7 @@ import { Component, OnInit } from '@angular/core';
 export class RawDataComponent implements OnInit {
   displayedColumns: string[] = ['PM10', 'PM25', 'S02', 'CO', 'O3','PB','HC','VOC','TEMP','HUMIDITY','PRESSURE','GPS','BATTERY','MEASURED','AQI'];
   dataSource = [
-    {'PM10':1, 'PM25':1, 'S02':1, 'CO':1, 'O3':1,'PB':1,'HC':1,'VOC':1,'TEMP':1,'HUMIDITY':16,'PRESSURE':1023,'GPS':'1121.121;1234.45','BATTERY':1,'MEASURED':1,'AQI':1}
+    {'PM10':1, 'PM25':1, 'S02':1, 'CO':1, 'O3':1,'PB':1,'HC':1,'VOC':1,'TEMP':1,'HUMIDITY':16,'PRESSURE':1023,'GPS':'1121.121.454545; 1234.45.345233','BATTERY':1,'MEASURED':1,'AQI':1}
   ];
 
   constructor() { }
