8afcf7b7afdd188d723edebccf7e8aba3b800ea9
hevenko <hevenko@gmail.com>
2020-10-07 00:06:06 +0200

    fix: closing filter to early or not at all

--- src/app/directive/dropdown.directive.ts
+++ src/app/directive/dropdown.directive.ts
@@ -8,17 +8,34 @@ export class DropdownDirective {
   @HostBinding('class.open') isOpen = false;
 
   constructor(private elRef: ElementRef) { }
-  /**
 
-  */
+  //closeMenu flag may be set on parent or higher up the DOM
+  shouldParentCloseTheMenu(target: any): boolean {
+    let result = false;
+    if (target.tagName.toUpperCase().indexOf("APP-") != -1) { //stop if this component root DOM is reached
+      result = false;
+    } else if (target.classList.contains('closeMenu')) {
+      result = true;
+    } else {
+      result = result || this.shouldParentCloseTheMenu(target.parentNode); // recursive parent search
+    }
+    return result
+  }
   @HostListener('document:click', ['$event']) toggleOpen(event: any) {
-      if (!!!this.isOpen) {
-        this.isOpen = this.elRef.nativeElement.contains(event.target) || this.elRef.nativeElement.classList.contains(Constants.STAY_OPEN);
-      } else if (this.elRef.nativeElement.contains(event.target)){
-        //toggle open and close when clicking on filter title
-        this.isOpen = !(this.elRef.nativeElement.firstChild.contains(event.target) || this.elRef.nativeElement.firstChild == event.target);
+
+      if (this.elRef.nativeElement.classList.contains(Constants.STAY_OPEN)) { // stayOpen flag is set on this component
+        this.isOpen = true;
+      } else if (this.elRef.nativeElement.contains(event.target)) { // clicked inside of this component
+        // clicked on component header
+        if (this.elRef.nativeElement.firstChild.contains(event.target) || this.elRef.nativeElement.firstChild == event.target) {
+          this.isOpen = !this.isOpen;
+        } else if (this.shouldParentCloseTheMenu(event.target)) { // click on item should close the menu
+          this.isOpen = false;
+        } else {
+          this.isOpen = true; // click was inside of this component
+        }
       } else {
-        this.isOpen = false;
+          this.isOpen = false; // click was outside of this component
       }
   }
 }
