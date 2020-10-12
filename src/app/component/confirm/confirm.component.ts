20f400fb1ff77c4c5f34da1ba7c01d2efa653144
hevenko <hevenko@gmail.com>
2020-10-12 18:36:44 +0200

    added confirm component to ask user for confirmation

---
+++ src/app/component/confirm/confirm.component.ts
@@ -0,0 +1,18 @@
+import { Component, OnInit, Inject } from '@angular/core';
+import {MatDialog, MAT_DIALOG_DATA} from '@angular/material/dialog';
+import {DialogData} from '../../shared/dialog-data';
+
+@Component({
+  selector: 'app-confirm',
+  templateUrl: './confirm.component.html',
+  styleUrls: ['./confirm.component.css']
+})
+export class ConfirmComponent implements OnInit {
+  title = 'Are you shure?';
+  constructor(@Inject(MAT_DIALOG_DATA) private data: DialogData) { }
+
+  ngOnInit(): void {
+    this.title = !!this.data ? this.data.title : this.title;
+  }
+
+}
