95cad4ef30dc9f569e23defb72b35b7dcee41dd4
hevenko <hevenko@gmail.com>
2020-10-12 19:18:56 +0200

    refactoring

--- src/app/component/user-devices/new-device/new-device.component.ts
+++ src/app/component/user-devices/new-device/new-device.component.ts
@@ -1,9 +1,6 @@
 import { Component, OnInit, Inject } from '@angular/core';
 import {MatDialog, MAT_DIALOG_DATA} from '@angular/material/dialog';
-
-export interface DialogData {
-  title: string;
-}
+import {DialogData} from '../../../shared/dialog-data';
 
 @Component({
   selector: 'app-new-device',
