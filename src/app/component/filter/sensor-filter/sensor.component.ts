21d80e728b73c729dd50f87f6a2d7789a9e9e05e
hevenko <hevenko@gmail.com>
2020-10-18 22:46:15 +0200

    tiny refactoring

--- src/app/component/filter/sensor-filter/sensor.component.ts
+++ src/app/component/filter/sensor-filter/sensor.component.ts
@@ -7,11 +7,11 @@ import {FormArray, FormControl, FormGroup, Validators} from '@angular/forms';
   styleUrls: ['./sensor.component.css']
 })
 export class SensorComponent implements OnInit {
-  values = [
-    {value: '1', desc: 'Temperature', label: '<sup>o</sup>C'},
-    {value: '2', desc: 'Humidity', label: 'H%'},
-    {value: '1', desc: 'Sulfur dioxide', label: 'SO<sub>2</sub>'},
-    {value: '2', desc: 'Ozone', label: 'O<sub>3</sub>'}
+  sensorList = [
+    {value: 'temp', desc: 'Temperature', label: '<sup>o</sup>C'},
+    {value: 'humidity', desc: 'Humidity', label: 'H%'},
+    {value: 'so2', desc: 'Sulfur dioxide', label: 'SO<sub>2</sub>'},
+    {value: 'o3', desc: 'Ozone', label: 'O<sub>3</sub>'}
   ];
   compForm: FormGroup = new FormGroup({});
   
@@ -22,7 +22,7 @@ export class SensorComponent implements OnInit {
   }
   initForm() {
     const faSensors: FormArray = new FormArray([]);
-    for (const def of this.values) {
+    for (const def of this.sensorList) {
       faSensors.push(new FormControl(false));
     }
     this.compForm = new FormGroup({
@@ -34,8 +34,16 @@ export class SensorComponent implements OnInit {
   }
   getLabel() {
     const label = this.compForm.value.sensors
-      .map((v, i) => (v ? i === 0 ? this.values[i].label :  ' ' + this.values[i].label : null))
+      .map((v, i) => (v ? i === 0 ? this.sensorList[i].label :  ' ' + this.sensorList[i].label : null))
       .filter(v => v !== null);
     return !!label && label.length > 0  ? label : 'Sensors';
+  }
+  getComponentValue(): string[] {
+    let result = [];
+    const label = this.compForm.value.sensors
+      .map((v, i) => (v ? this.sensorList[i].value :  null))
+      .filter(v => v !== null);
+    return result
+
   }
 }
